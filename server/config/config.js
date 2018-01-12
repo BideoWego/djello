5feeee2f4f2bfe7995241b2bc3d1e38678c05420
Bideo Wego <bideowego@gmail.com>
2018-01-11 22:00:27 -0500

    Update db config and install pg

--- server/config/config.js
+++ server/config/config.js
@@ -2,14 +2,14 @@ module.exports = {
   "development": {
     "username": process.env.PG_USERNAME,
     "password": null,
-    "database": "project_mimirs_market_development",
+    "database": "project_djello_development",
     "host": "127.0.0.1",
     "dialect": "postgres"
   },
   "test": {
     "username": process.env.PG_USERNAME,
     "password": null,
-    "database": "project_mimirs_market_test",
+    "database": "project_djello_test",
     "host": "127.0.0.1",
     "dialect": "postgres"
   },
